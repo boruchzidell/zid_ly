1067632608463e3cb2fd625b760da927e256cc68
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2023-02-10 14:27:33 -0500

    add seed_data.sql

---
+++ seed_data.sql
@@ -0,0 +1,13 @@
+drop table if exists urls;
+
+create table urls (
+  id serial primary key ,
+  hex varchar(6) not null unique,
+  original_url varchar(2048) not null,
+  created text default to_char(timezone('EST', current_timestamp(0)), 'YYYY-MM-DD HH12:MI:SS am')
+);
+
+insert into urls (hex, original_url)
+values('aaa', 'http://www.google.com');
+
+-- select * from urls u ;
