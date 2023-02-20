0890280b6206f53d159270c681380eb6a3bcceae
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2023-02-20 15:02:53 -0500

    functions as intended

--- app.js
+++ app.js
@@ -1,25 +1,64 @@
 let express = require('express');
 let app = express();
+
+let morgan = require('morgan');
+app.use(morgan('tiny'));
+
+app.use(express.static('public'));
+
 require('dotenv').config();
 let {pool} = require('./database_config.js');
 
-app.use(express.static('public'));
+app.use(express.json());
 
 function generateHex() {
   const max = 16777215; // Hex 6 digits
   return Math.floor(Math.random() * max).toString(16);
 }
 
-app.get('/test/:hex', (req, res, next) => {
+// function isValidUrl(string) {
+//   //TODO fix this
+//   try {
+//     new URL(string)
+//       return true;
+//     } catch (err) {
+//       return false;
+//     }
+// }
+
+
+app.get('/:hex', (req, res, next) => {
   let hex = req.params.hex;
 
   pool.query('select original_url from urls where hex = $1', [hex], (err, results) => {
-    res.redirect(results.rows[0].original_url);
+    if (err) {
+      console.log('Hex not found');
+      res.status(404).send('Hex not found');
+    } else {
+      res.redirect(results.rows[0].original_url);
+    }
   });
 });
 
-app.get('/', (req, res, next) => {
-  res.send('Zid.ly homepage');
+// app.get('/', (req, res, next) => {
+//   res.send('Zid.ly homepage');
+// });
+
+app.post('/', (req, res, next) => {
+  let url = req.body.url;
+  console.log(url);
+
+  pool.query('insert into urls (hex, original_url) values ($1, $2) returning hex;', [generateHex(), url], (err, results) => {
+    if (err) {
+      // throw new Error('Nope!');
+      console.error('Nope!');
+    } else {
+      // res.send(results[0].hex)
+      console.log(results.rows[0].hex);
+      res.send(results.rows[0].hex);
+    }
+  });
+
 });
 
 let port = process.env.PORT || 5000;
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,9 +1,96 @@
-document.addEventListener('DOMContentLoaded', () => {
-  let form = document.getElementById('url-form');
 
-  form.addEventListener('submit', (event) => {
+
+async function submitForm(event) {
     event.preventDefault();
 
-    console.log('hi');
-  });
+    let originalUrl = this.elements[0].value;
+
+    let response = await fetch('/', {
+      method: 'POST',
+      body: JSON.stringify({url: originalUrl}),
+      headers: { "Content-Type": "application/json" }
+    });
+
+    let hex = await response.text();
+
+    let container = document.getElementById('container');
+
+    let link = container.querySelector('a');
+    link.setAttribute('href', `/${hex}`);
+    link.textContent = `${window.location.href}${hex}`;
+
+    document.querySelector('input[type=submit]').disabled = true;
+    document.querySelector('#original-url').setAttribute('readonly', true);
+
+    document.querySelectorAll('.hidden').forEach(el => el.className = 'show')
+
+  }
+
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('url-form').addEventListener('submit', submitForm);
+
+  document.body.addEventListener('click', function(event) {
+    let button = event.target;
+
+    if (button.id === 'copy-btn') {
+      let text = document.getElementById('shortened-url').textContent
+      window.navigator.clipboard.writeText(text);
+
+      button.innerText = 'Copied';
+
+      // button.textContent = '  Copied   ';
+      button.disabled = true;
+
+      setTimeout(() => {
+        button.textContent = 'Copy';
+        button.disabled = false;
+        // button.style.background = 'lightgray';
+      }, 2000)
+    }
+  // });
+
+  // for (let i = 0; i < codes.length; i += 1) {
+  //   let button = document.createElement('button');
+  //   button.classList.add('copy_btn');
+  //   button.textContent = 'Copy code';
+  //   button.style.float = 'right';
+  //   /*Makes button stay on top of other elements, and only works on positioned elements (absolute, relative, etc)*/
+  //   button.style.position = 'relative'
+  //   button.style.zIndex = '150';
+
+  //   button.style.background = 'lightgray';
+  //   button.style.color = 'black'
+  //   button.style.fontSize = '.8em';
+  //   button.style.minWidth = '7em';
+  //   button.style.minHeight = '2em';
+  //   button.style.cursor = 'pointer';
+  //   button.style.margin = '0px';
+  //   button.style.padding = '0px';
+
+  //   codes[i].prepend(button);
+
+  //   // Delete button on right-click
+  //   button.addEventListener('contextmenu', function(event) {
+  //     event.preventDefault();
+  //     this.remove();
+  //   });
+
+    // Simulate :hover
+    // button.addEventListener('mouseover', function() {
+    //   this.style.background = 'darkgray';
+    // });
+
+    // button.addEventListener('mouseleave', function() {
+    //   this.style.background = 'lightgray'
+    // });
+  })
 });
+
+
+
+// let url = 'http://localhost:5000/users'
+// fetch(url, {
+//     method: "POST",
+//     body: JSON.stringify({username: "Demo"}),
+//     headers: {"Content-Type": "application/json"}
+// }).then(res => res.json()).then(resBody => console.log(resBody));
