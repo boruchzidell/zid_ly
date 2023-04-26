8e42aca56b7d2f90113c899b2a62f6006ce9582f
Boruch Zidell <47635324+boruchzidell@users.noreply.github.com>
2023-04-26 17:21:09 -0400

    Change table schema set the default for the create column to respect DST

--- seed_data.sql
+++ seed_data.sql
@@ -4,10 +4,10 @@ create table urls (
   id serial primary key ,
   hex varchar(6) not null unique,
   original_url varchar(2048) not null,
-  created text default to_char(timezone('EST', current_timestamp(0)), 'YYYY-MM-DD HH12:MI:SS am')
+  created text default to_char(timezone('America/New_york', current_timestamp(0)), 'YYYY-MM-DD HH12:MI:SS am')
 );
 
 insert into urls (hex, original_url)
 values('aaa', 'http://www.google.com'), ('bbb', 'http://www.amazon.com'), ('fly', 'https://fly.io/apps/baz-test/monitoring');
 
-select * from urls u ;
+-- select * from urls u ;
